e5259c3de8a1841c9bd03c4b453c358bb557c5c2
Pedro Igor <pedroigor.silva@outlook.com>
2021-04-06 23:58:54 -0300

    Update GPIO ports

--- trabalho2/src/main.c
+++ trabalho2/src/main.c
@@ -1,24 +1,24 @@
 #include "gpio.h"
 
-#define LAMPADA_1_OUT 17
-#define LAMPADA_2_OUT 18
-#define LAMPADA_3_OUT 27
-#define LAMPADA_4_OUT 22
-#define AR_COND_1_OUT 13
-#define AR_COND_2_OUT 19
-#define SENSOR_PRESENCA_1_IN 25
-#define SENSOR_PRESENCA_2_IN 26
-#define SENSOR_ABERTURA_1_IN 5
-#define SENSOR_ABERTURA_2_IN 6
-#define SENSOR_ABERTURA_3_IN 12
-#define SENSOR_ABERTURA_4_IN 16
-#define SENSOR_ABERTURA_5_IN 20
-#define SENSOR_ABERTURA_6_IN 21
+#define LAMPADA_1_OUT 0
+#define LAMPADA_2_OUT 1
+#define LAMPADA_3_OUT 2
+#define LAMPADA_4_OUT 3
+#define AR_COND_1_OUT 23
+#define AR_COND_2_OUT 24
+#define SENSOR_PRESENCA_1_IN 6
+#define SENSOR_PRESENCA_2_IN 25
+#define SENSOR_ABERTURA_1_IN 21
+#define SENSOR_ABERTURA_2_IN 22
+#define SENSOR_ABERTURA_3_IN 26
+#define SENSOR_ABERTURA_4_IN 27
+#define SENSOR_ABERTURA_5_IN 28
+#define SENSOR_ABERTURA_6_IN 29
 
 int main(int argc, const char * argv[]) {
     printf("%d\n", GPIO());
     while(1){
-        read_gpio(SENSOR_PRESENCA_1_IN);
-        usleep(700000);
+        read_gpio(SENSOR_PRESENCA_2_IN);
+        sleep(3);
     };
 }
