24dc708360329b3fedfe2edeb96e25481e6b6cad
Pedro Igor <pedroigor.silva@outlook.com>
2021-04-17 12:37:41 -0300

    Improve gpio based on trabalho1 feedback

--- trabalho2/server/src/gpio.c
+++ trabalho2/server/src/gpio.c
@@ -1,5 +1,6 @@
 #include "gpio.h"
 #include "json.h"
+#include "client.h"
 #define LAMPADA_1_OUT 0
 #define LAMPADA_2_OUT 1
 #define LAMPADA_3_OUT 2
@@ -18,18 +19,21 @@
 int HANDLER_PTHREAD_ID;
 void GPIO(){
     wiringPiSetup ();
+    int ports[] = {LAMPADA_1_OUT, LAMPADA_2_OUT, LAMPADA_3_OUT, LAMPADA_4_OUT, AR_COND_1_OUT, AR_COND_2_OUT};
+    int length = sizeof(ports)/sizeof(ports[0]);
+    for(int i = 0; i < length; i++){
+        pinMode(ports[i], OUTPUT);
+        digitalWrite(ports[i], LOW);
+    }
     create_handlers();
 }
 void turn_on(int gpio_port){
-    pinMode(gpio_port, OUTPUT);
     digitalWrite(gpio_port, HIGH); 
 }
 void turn_off(int gpio_port){
-    pinMode(gpio_port, OUTPUT);
     digitalWrite(gpio_port, LOW);
 }
 int read_gpio(int gpio_port){
-    pinMode(gpio_port, INPUT);
     int value = digitalRead(gpio_port);
     return value;
 }
@@ -45,72 +49,72 @@ void TURN_OFF_GPIO(){
 void handle_gpio_component_0() {
     presence_sensors[0].value = read_gpio(presence_sensors[0].port);
     printf("porta: %d valor: %d\n", presence_sensors[0].port, presence_sensors[0].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_1() {
     presence_sensors[1].value = read_gpio(presence_sensors[1].port);
     printf("porta: %d valor: %d\n", presence_sensors[1].port, presence_sensors[1].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_2() {
     open_sensors[0].value = read_gpio(open_sensors[0].port);
     printf("porta: %d valor: %d\n", open_sensors[0].port, open_sensors[0].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_3() {
     open_sensors[1].value = read_gpio(open_sensors[1].port);
     printf("porta: %d valor: %d\n", open_sensors[1].port, open_sensors[1].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_4() {
     open_sensors[2].value = read_gpio(open_sensors[2].port);
     printf("porta: %d valor: %d\n", open_sensors[2].port, open_sensors[2].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_5() {
     open_sensors[3].value = read_gpio(open_sensors[3].port);
     printf("porta: %d valor: %d\n", open_sensors[3].port, open_sensors[3].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_6() {
     open_sensors[4].value = read_gpio(open_sensors[4].port);
     printf("porta: %d valor: %d\n", open_sensors[4].port, open_sensors[4].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_7() {
     open_sensors[5].value = read_gpio(open_sensors[5].port);
     printf("porta: %d valor: %d\n", open_sensors[5].port, open_sensors[5].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_8() {
     light_outs[0].value = read_gpio(light_outs[0].port);
     printf("porta: %d valor: %d\n", light_outs[0].port, light_outs[0].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_9() {
     light_outs[1].value = read_gpio(light_outs[1].port);
     printf("porta: %d valor: %d\n", light_outs[1].port, light_outs[1].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_10() {
     light_outs[2].value = read_gpio(light_outs[2].port);
     printf("porta: %d valor: %d\n", light_outs[2].port, light_outs[2].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_11() {
     light_outs[3].value = read_gpio(light_outs[3].port);
     printf("porta: %d valor: %d\n", light_outs[3].port, light_outs[3].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_12() {
     air_outs[0].value = read_gpio(air_outs[0].port);
     printf("porta: %d valor: %d\n", air_outs[0].port, air_outs[0].value);
-    get_json();
+    send_message(get_json());
 }
 void handle_gpio_component_13() {
     air_outs[1].value = read_gpio(air_outs[1].port);
     printf("porta: %d valor: %d\n", air_outs[1].port, air_outs[1].value);
-    get_json();
+    send_message(get_json());
 }
 void create_handlers () {
     int presence_ports[] = {SENSOR_PRESENCA_1_IN, SENSOR_PRESENCA_2_IN};
@@ -123,21 +127,25 @@ void create_handlers () {
     int length_light = sizeof(light_ports)/sizeof(light_ports[0]);
     int length_air = sizeof(air_ports)/sizeof(air_ports[0]);
     for(int i = 0; i < length_presence; i++){
+        pinMode(presence_ports[i], INPUT);
         presence_sensors[i].port = presence_ports[i];
         presence_sensors[i].value = read_gpio(presence_ports[i]);
         wiringPiISR(presence_ports[i], INT_EDGE_BOTH, functions[i]);
     }
     for(int i = 0; i <length_open; i++){
+        pinMode(open_ports[i], INPUT);
         open_sensors[i].port = open_ports[i];
         open_sensors[i].value = read_gpio(open_ports[i]);
         wiringPiISR(open_ports[i], INT_EDGE_BOTH, functions[i+length_presence]);
     }
     for(int i = 0; i <length_light; i++){
+        pinMode(light_ports[i], INPUT);
         light_outs[i].port = light_ports[i];
         light_outs[i].value = read_gpio(light_ports[i]);
         wiringPiISR(light_ports[i], INT_EDGE_BOTH, functions[i+length_presence+length_open]);
     }
     for(int i = 0; i <length_air; i++){
+        pinMode(air_ports[i], INPUT);
         air_outs[i].port = air_ports[i];
         air_outs[i].value = read_gpio(air_ports[i]);
         wiringPiISR(air_ports[i], INT_EDGE_BOTH, functions[i+length_presence+length_open+length_light]);
