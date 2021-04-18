e8d17781a8134d348564fad6d51debcebd9e511d
Pedro Igor <pedroigor.silva@outlook.com>
2021-04-18 17:50:08 -0300

    Add create csv by date

---
+++ trabalho2/central/inc/csv.h
@@ -0,0 +1,13 @@
+#ifndef CSV_H_
+#define CSV_H_
+#include<stdio.h>
+#include<string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+void CSV();
+void insert_line(float values[], int size);
+#endif
