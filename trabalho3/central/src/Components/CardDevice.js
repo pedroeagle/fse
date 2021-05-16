dc7c2abba53c277e1bef44de222cce1a0e41fc49
Pedro Igor <pedroigor.silva@outlook.com>
2021-05-16 16:46:02 -0300

    Add unity to showed info
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

--- trabalho3/central/src/Components/CardDevice.js
+++ trabalho3/central/src/Components/CardDevice.js
@@ -25,16 +25,15 @@ function CardDevice({ devices, modo, subscribe, comodoHost, client, devicesInfo,
                 subscribeToChannels(device.comodo);
                 return (
                     <Grid item md={2} key={index} className="list">
-                        {console.log(devicesInfo)}
                         <Card className={modo=='energia'?'energyCard':'batteryCard'}>
                             <Typography variant="h4">{device.comodo}</Typography>
                             <Typography variant="h6">Modo: {modo}</Typography>
-                            <Typography variant="h6">{device.entrada} : {devicesInfo[device.comodo]?.estado?.entrada}</Typography>
+                            <Typography variant="h6">{device.entrada} : {devicesInfo[device.comodo]?.estado?.entrada.toString()}</Typography>
                             {modo === "energia" ?
                                 <>
-                                    <Typography variant="h6">{device.saida} : {devicesInfo[device.comodo]?.estado?.saida}</Typography>
-                                    <Typography variant="h6" >Umidade: {devicesInfo[device.comodo]?.umidade}</Typography>
-                                    <Typography variant="h6" >Temperatura: {devicesInfo[device.comodo]?.temperatura}</Typography>
+                                    <Typography variant="h6">{device.saida} : {devicesInfo[device.comodo]?.estado?.saida.toString()}</Typography>
+                                    <Typography variant="h6" >Umidade: {devicesInfo[device.comodo]?.umidade} %</Typography>
+                                    <Typography variant="h6" >Temperatura: {devicesInfo[device.comodo]?.temperatura} ºC</Typography>
                                 </> : null}
                             <Typography variant="h8">MAC: {device.device}</Typography><br/>
                             <Button className="removeButton" onClick={(e)=>{remove(client, device, modo)}}>Remover dispositivo</Button>
