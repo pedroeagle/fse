de7cec89fe7197a5e0b61120b638b9cbe5b3cb3f
Pedro Igor <pedroigor.silva@outlook.com>
2021-05-19 02:15:40 -0300

    Fix CSV

--- trabalho3/central/src/Components/CSV.js
+++ trabalho3/central/src/Components/CSV.js
@@ -2,10 +2,11 @@ import { CSVLink } from "react-csv";
 import "./CSV.css"
 
 const CSV = ({ data }) => {
+    // eslint-disable-next-line
     const headers = [{ key: "evento" }, { key: "dispositivo" }, { key: "horario" }];
     return (
         <div className="downloadDiv">
-            {data.length > 0 ? <CSVLink headers={headers} className="downloadLink" data={data} filename="Logs - Central de Controle.csv">Download Logs</CSVLink> : ''}
+            {data.length > 0 ? <CSVLink className="downloadLink" data={data} filename="Logs - Central de Controle.csv">Download Logs</CSVLink> : ''}
         </div>
     );
 }
